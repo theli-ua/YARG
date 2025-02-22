da6984b7dbc43c7a83b914bcec1c78fb6f735060
Anton Romanov <theli.ua@gmail.com>
2025-02-22 12:31:39 -0800

    Remove all setfade references

--- Assets/Script/Gameplay/Visuals/TrackElements/NoteFlare.cs
+++ Assets/Script/Gameplay/Visuals/TrackElements/NoteFlare.cs
@@ -13,10 +13,6 @@ namespace YARG.Gameplay.Visuals
         private float _scaleConstant = 4.25f;
         private LensFlareComponentSRP _flare;
 
-        private float _fadeFullPosition;
-        private float _fadeZeroPosition;
-        private float _fadeSize;
-
         private void Awake()
         {
             _flare = GetComponent<LensFlareComponentSRP>();
@@ -27,28 +23,10 @@ namespace YARG.Gameplay.Visuals
             _flare.intensity = 0f;
         }
 
-        public void SetFade(float fadePos, float fadeSize)
-        {
-            _fadeFullPosition = fadePos - fadeSize;
-            _fadeZeroPosition = fadePos;
-            _fadeSize = fadeSize;
-        }
-
         private void Update()
         {
-            // Update flare intensity based on fade
-
             var pos = transform.position.z;
             float intensity = 1f;
-
-            if (pos >= _fadeZeroPosition)
-            {
-                intensity = 0f;
-            }
-            else if (pos > _fadeFullPosition)
-            {
-                intensity = 1f - (pos - _fadeFullPosition) / _fadeSize;
-            }
 
             // We use "EaseInOutCubic" as the track fade also uses that
             _flare.intensity = EaseInOutCubic(Mathf.Clamp01(intensity));
